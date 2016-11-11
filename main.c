8a2ddfd92edab6f2864a9fdebcda236e1bb65892
Ethan Mahintorabi <EthanMahintorabi@yahoo.com>
2016-11-10 19:37:18 -0800

    Added Semaphore Structure

--- main.c
+++ main.c
@@ -9,6 +9,13 @@ Producer consumer problem.
 #include <sys/stat.h>
 #include <semaphore.h>
 
+
+const char* FULL_NAME = "/depalmaFull";
+const char* EMPTY_NAME = "/depalmaFull";
+
+sem_t* mutexFull;
+sem_t* mutexEmpty;
+
 // Constants
 #define PROD 1
 #define CONS 0
@@ -22,5 +29,17 @@ void consumer() {
 void criticalSection(int who) {
 }
 
+void createSemaphores(int intialValueFull){
+  mutexFull = sem_open(FULL_NAME, O_CREAT, S_IRWXG, intialValueFull);
+  mutexEmpty = sem_open(EMPTY_NAME, O_CREAT, S_IRWXG, 0);
+}
+
+void closeSemaphores(){
+  sem_close(mutexFull);
+  sem_close(mutexEmpty);
+}
+
 int main() {
+  createSemaphores(100);
+  closeSemaphores();
 }
