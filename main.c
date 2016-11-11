5bcdc75dd7e37122615044418ea9fb9edbeff7c4
Kyle McCrohan <kmccrohan@zagmail.gonzaga.edu>
2016-11-10 19:16:24 -0800

    init commit

---
+++ main.c
@@ -0,0 +1,26 @@
+/**
+Producer consumer problem.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <semaphore.h>
+
+// Constants
+#define PROD 1
+#define CONS 0
+
+void producer() {
+}
+
+void consumer() {
+}
+
+void criticalSection(int who) {
+}
+
+int main() {
+}
